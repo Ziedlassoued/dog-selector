2bbe02eaf190af75c4277c330f98ef2f9c6be09d
ziedlassoued <zied.lassoued@outlook.de>
2021-10-22 10:59:17 +0200

    :sparkles: Add mainApp

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+import { createElement } from './lib/element';
 import './style.css';
+
+function mainApp() {
+  const appElement = document.querySelector('#app');
+  const headerElement = createElement(
+    'header',
+    {
+      className: 'header',
+    },
+    [
+      createElement('h1', {
+        textContent: 'Dogs Random Selector',
+      }),
+    ]
+  );
+
+  const mainElement = createElement(
+    'main',
+    {
+      className: 'main',
+    },
+    [
+      createElement('p', {
+        textContent: 'Please wait...',
+      }),
+    ]
+  );
+
+  appElement.append(headerElement, mainElement);
+}
+mainApp();
